036aa542288714aa03d7637df8e51731c5a94600
Hugo <firefactoryan@gmail.com>
2020-08-17 21:00:03 -0300

    Added main class

--- src/kotlin/Exercise.kt
+++ src/kotlin/Exercise.kt
@@ -5,4 +5,9 @@ import java.util.*
 fun main() {
     val hugo = KotlinPerson(1L, "Mr", "Hugo", "Silva",  GregorianCalendar(1985,4,19));
     val dayane = KotlinPerson(1L, "Mrs", "Dayane", "Gimenes",  GregorianCalendar(1985,10,13));
+
+    println("$hugo s age is ${hugo.getAge()}")
+    println("$dayane s age is ${dayane.getAge()}")
+    println("The age of someone born on 3rd May 1988 is ${KotlinPerson.getAge(GregorianCalendar(1985,4,19))}")
+
 }
--- src/kotlin/KotlinPerson.kt
+++ src/kotlin/KotlinPerson.kt
@@ -1,6 +1,6 @@
 package kotlin
 
-import java.util.Calendar
+import java.util.*
 
 class KotlinPerson(
     private val id: Long,
@@ -10,5 +10,32 @@ class KotlinPerson(
     private val dateOfBirth: Calendar
 ) {
 
-    override fun toString() = " ${this.title}  ${this.firstName}  ${this.surname}"
+    override fun toString() = "${this.title}  ${this.firstName}  ${this.surname}"
+
+    override fun equals(other: Any?): Boolean {
+
+
+                return true
+    }
+
+    override fun hashCode() = Objects.hash(id, title, firstName, surname)
+
+    companion object{
+        fun getAge(dateOfBirth: Calendar?): Int{
+            val today = GregorianCalendar()
+            val years = today.get(Calendar.YEAR) - dateOfBirth?.get(Calendar.YEAR)!!
+
+            if(dateOfBirth.get(Calendar.DAY_OF_YEAR) >= today.get(Calendar.YEAR)){
+                return years - 1
+            }
+            else{
+                return years
+            }
+
+        }
+    }
+
+    fun getAge() = getAge(dateOfBirth)
 }
+
+
