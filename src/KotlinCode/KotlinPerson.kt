1a466ea602bb4f8d018c541092cd43f60e07cadb
Hugo <firefactoryan@gmail.com>
2020-08-17 21:07:54 -0300

    Added final exercise

--- src/KotlinCode/KotlinPerson.kt
+++ src/KotlinCode/KotlinPerson.kt
@@ -1,27 +1,26 @@
-package kotlin
+package KotlinCode
 
-import java.util.*
+import java.util.Calendar
+import java.util.GregorianCalendar
+import java.util.Objects
+
+
+
 
 class KotlinPerson(
     private val id: Long,
     private val title: String,
     private val firstName: String,
     private val surname: String,
-    private val dateOfBirth: Calendar
+    private val dateOfBirth: Calendar?
 ) {
 
-    override fun toString() = "${this.title}  ${this.firstName}  ${this.surname}"
-
-    override fun equals(other: Any?): Boolean {
-
-
-                return true
-    }
+    override fun toString() = "$title $firstName $surname"
 
     override fun hashCode() = Objects.hash(id, title, firstName, surname)
 
     companion object{
-        fun getAge(dateOfBirth: Calendar?): Int{
+        fun getAge(dateOfBirth: Calendar?): Int {
             val today = GregorianCalendar()
             val years = today.get(Calendar.YEAR) - dateOfBirth?.get(Calendar.YEAR)!!
 
