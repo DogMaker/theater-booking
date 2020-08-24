b9c246dbbfa401709181d7e6e5e50edff21e0f4a
Hugo <firefactoryan@gmail.com>
2020-08-24 20:51:54 -0300

    Fixed statement

--- src/KotlinCode/KotlinPerson.kt
+++ src/KotlinCode/KotlinPerson.kt
@@ -21,13 +21,7 @@ class KotlinPerson(
             val today = GregorianCalendar()
             val years = today.get(Calendar.YEAR) - dateOfBirth?.get(Calendar.YEAR)!!
 
-            if(dateOfBirth.get(Calendar.DAY_OF_YEAR) >= today.get(Calendar.YEAR)){
-                return years - 1
-            }
-            else{
-                return years
-            }
-
+            return if(dateOfBirth.get(Calendar.DAY_OF_YEAR) >= today.get(Calendar.YEAR)) years - 1 else years
         }
     }
     fun getAge() = getAge(dateOfBirth)
